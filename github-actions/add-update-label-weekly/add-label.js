d3c105b219d2bcc7a4ed513c2922e2cbc72fc844
Saumil Dhankar <saumil.dhankar@yahoo.com>
2021-10-15 20:17:54 -0700

    Update add-label.js

--- github-actions/add-update-label-weekly/add-label.js
+++ github-actions/add-update-label-weekly/add-label.js
@@ -43,7 +43,7 @@ async function main({ g, c }, columnId) {
 		// Note: inactive label is added as well if the timeline indicates the issue is inactive. Further, the if else structure ensures addLabels commands are limited.
 		// 
 		
-		const responseObject = isTimelineOutdated(timeline, issueNum, assignees)
+		const responseObject = await isTimelineOutdated(timeline, issueNum, assignees)
 		if (responseObject.result === true) {
 			console.log(`Going to ask for an update now for issue #${issueNum}`);
 			await removeLabels(issueNum, responseObject.label);  
