7381e061a8634d4645364208321c3aa4be080bb5
Saumil Dhankar <saumil.dhankar@yahoo.com>
2021-10-17 08:37:06 -0700

    Update add-label.js

--- github-actions/add-update-label-weekly/add-label.js
+++ github-actions/add-update-label-weekly/add-label.js
@@ -151,7 +151,7 @@ async function* getTimeline(issueNum) {
 
 async function isTimelineOutdated(timeline, issueNum, assignees) {
 	for await (let moment of timeline) {
-		if (isMomentRecent(moment.created_at, cutoffTime) === true && isMomentRecent(moment.created_at, cutoffTime1) === false) {
+		if (isMomentRecent(moment.created_at, cutoffTime) && isMomentRecent(moment.created_at, cutoffTime1) === false) {
 			if (moment.event == 'cross-referenced' && isLinkedIssue(moment, issueNum)) {
 				return {result: false, label: ['Status: Updated']}
 			}
