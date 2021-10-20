e94f9985541fdc446193c1ce9ee01d3db8f7776c
Saumil Dhankar <saumil.dhankar@yahoo.com>
2021-10-20 16:39:12 -0700

    Update add-label.js

--- github-actions/add-update-label-weekly/add-label.js
+++ github-actions/add-update-label-weekly/add-label.js
@@ -162,9 +162,17 @@ async function isTimelineOutdated(timeline, issueNum, assignees) {
 				return {result: true, labels: toUpdateLabel}
 			}
 		}
-		else*/ if (isMomentRecent(moment.created_at, zeroDayCutoffTime)) {
-			return {result: false, labels: statusUpdatedLabel}
-		}	
+		else*/ if (isMomentRecent(moment.created_at, fourteenDayCutoffTime)) {
+			if (moment.event == 'cross-referenced' && isLinkedIssue(moment, issueNum)) {
+				return {result: false, labels: statusUpdatedLabel}
+			}
+			else if (moment.event == 'commented' && isCommentByAssignees(moment, assignees)) {
+				return {result: false, labels: statusUpdatedLabel}
+			}
+			else {
+				return {result: true, labels: inactiveLabel}
+			}
+		}
 		else if (isMomentRecent(moment.created_at, threeDayCutoffTime)) {
 			if (moment.event == 'cross-referenced' && isLinkedIssue(moment, issueNum)) {
 				return {result: false, labels: statusUpdatedLabel}
@@ -176,17 +184,9 @@ async function isTimelineOutdated(timeline, issueNum, assignees) {
 				return {result: true, labels: toUpdateLabel}
 			}
 		}
-		else if (isMomentRecent(moment.created_at, fourteenDayCutoffTime)) {
-			if (moment.event == 'cross-referenced' && isLinkedIssue(moment, issueNum)) {
-				return {result: false, labels: statusUpdatedLabel}
-			}
-			else if (moment.event == 'commented' && isCommentByAssignees(moment, assignees)) {
-				return {result: false, labels: statusUpdatedLabel}
-			}
-			else {
-				return {result: true, labels: inactiveLabel}
-			}
-		}
+		else if (isMomentRecent(moment.created_at, zeroDayCutoffTime)) {
+			return {result: false, labels: statusUpdatedLabel}
+		}	
 	}
 }	
 
