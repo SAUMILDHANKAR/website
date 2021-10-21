f1ac7681982e7a09de248424c68c5b4499b2d4ad
Saumil Dhankar <saumil.dhankar@yahoo.com>
2021-10-21 16:41:00 -0700

    Update add-label.js

--- github-actions/add-update-label-weekly/add-label.js
+++ github-actions/add-update-label-weekly/add-label.js
@@ -43,15 +43,20 @@ async function main({ g, c }, columnId) {
 		// 
 		
 		const responseObject = await isTimelineOutdated(timeline, issueNum, assignees)
-		if (responseObject.result === true) {
+		if (responseObject.result === true && responseObject.labels === toUpdateLabel) {
 			console.log(`Going to ask for an update now for issue #${issueNum}`);
-			await removeLabels(issueNum, responseObject.labels);  
+			await removeLabels(issueNum, statusUpdatedLabel);  
 			await addLabels(issueNum, responseObject.labels); 
 			//await postComment(issueNum, assignees);
-		} else {
-			console.log(`No updates needed for issue #${issueNum}`);
-			await removeLabels(issueNum, responseObject.labels);
+		} else if (responseObject.result === true && responseObject.labels === statusUpdatedLabel) {
+			await removeLabels(issueNum, toUpdateLabel, inactiveLabel);
 			await addLabels(issueNum, responseObject.labels);
+		} else if (responseObject.result === true && responseObject.labels === inactiveLabel) {
+			await removeLabels(issueNum, statusUpdatedLabel);
+			await addLabels(issueNum, responseObject.labels);
+		} 
+		else {
+			console.log(`No updates needed for issue #${issueNum}`);
 		}
 		
 		/**
