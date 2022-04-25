66c5dfc7ea08128542b17c85424142ea351aebcc
Saumil Dhankar <saumil.dhankar@yahoo.com>
2022-04-25 14:44:02 -0700

    Update preliminary-update-comment.js

--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -68,7 +68,7 @@ async function getTimeline(issueNum) {
 async function lastAssignee(timeline) {
 	for await (let [index, moment] of timeline.entries()) {
 		if (moment.event == 'assigned') { 
-			return {moment.event.assignee.login}
+			return {login}
 		}
         	else {
             		break
