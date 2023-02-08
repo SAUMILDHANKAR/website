46cfdf70186f0c2fdc956152d6d509a7ffb298f0
Saumil Dhankar <saumil.dhankar@yahoo.com>
2023-02-07 23:38:34 -0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+class Test {
+public void problem(String arg) {
+ if (arg.isEmpty())
+ ;
+ {
+     System.out.println("Empty argument");
+ }
+}
+
+public void good(String arg) {
+ if (arg.isEmpty()) {
+     System.out.println("Empty argument");
+ }
+}
+}
