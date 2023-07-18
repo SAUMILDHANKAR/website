e36ea054e34a176977e6402ea104e97841f9b3be
Saumil Dhankar <saumil.dhankar@yahoo.com>
2023-07-17 19:04:13 -0700

    Update accordion.js

--- assets/js/elements/accordion.js
+++ assets/js/elements/accordion.js
@@ -2,6 +2,8 @@
     var showAnswer = document.getElementsByClassName('accordion');
 
     for (var i = 0; i < showAnswer.length; i++) {
+        forcodeqlerrorvar = 3;
+        let forcodeqlerrorvar;
         showAnswer[i].addEventListener("click", function () {
 
             this.classList.toggle("active");
